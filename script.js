c6453da945eeea9367a4b0a7ccd4010d68af1448
Colin James Nguyen <nguyen_c9@denison.edu>
2025-07-15 21:36:03 +0000

    logic is sort of working

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ const button = document.getElementById('presser');
 
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-const score = 0;
+let score = 0;
 
 button.addEventListener('keydown', startGame);
 
-
-function startGame() {
+function startGame(event) {
     const randomLetter = letters[Math.floor(Math.random() * letters.length)];
     button.textContent = randomLetter;
     console.log(randomLetter);
-}
+    const keyPressed = event.key;
+    if (keyPressed.toUpperCase() === randomLetter.toUpperCase()) {
+        score++;
+    }
+    console.log(`Score: ${score}`);
+};
 
