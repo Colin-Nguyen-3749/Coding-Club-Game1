f5597b74605aeb18dd191a4c73384a5227c5ff99
Colin James Nguyen <nguyen_c9@denison.edu>
2025-08-01 14:31:31 +0000

    Background color now changes everytime a button is pressed

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 console.log('JavaScript file is linked correctly.');
 
 const button = document.getElementById('presser');
+const body = document.body;
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#A1FF33'];
+let colorIndex = 0;
 let score = 0;
 
 let currentLetter = letters[Math.floor(Math.random() * letters.length)];
@@ -11,6 +14,8 @@ button.addEventListener('keydown', playGame);
 
 function playGame(event) {
     const keyPressed = event.key;
+    body.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length
     if (keyPressed.toUpperCase() === currentLetter.toUpperCase()) {
         score++;
         console.log(`Correct! Score: ${score}`);
