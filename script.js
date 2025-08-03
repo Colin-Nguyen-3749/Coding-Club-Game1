48c9348168277fb9966869d2b4266842b675d795
Colin James Nguyen <nguyen_c9@denison.edu>
2025-08-03 20:29:39 +0000

    Buttons now briefly change the correct colors

--- script.js
+++ script.js
@@ -17,20 +17,28 @@ function playGame(event) {
         button.classList.add('clicked');
     });
 
+    if (keyPressed.toUpperCase() === currentLetter.toUpperCase()) {
+        console.log('eureka');
+        button.classList.add('correct');
+        setTimeout(() => {
+            button.classList.remove('correct');
+        }, 200);
+        score++;
+        console.log(`Correct! Score: ${score}`);
+
+    } else {
+        button.classList.add('wrong');
+        
+        console.log(`Wrong! Score: ${score}`);
+    }
+    currentLetter = letters[Math.floor(Math.random() * letters.length)];
+    button.textContent = currentLetter;
+
     document.addEventListener('keyup', (e) => {
         button.classList.remove('clicked');
     });
 
 
 
-    if (keyPressed.toUpperCase() === currentLetter.toUpperCase()) {
-        button.classList.toggle('correct');
-        score++;
-        console.log(`Correct! Score: ${score}`);
-    } else {
-        button.classList.toggle('wrong');
-        console.log(`Wrong! Score: ${score}`);
-    }
-    currentLetter = letters[Math.floor(Math.random() * letters.length)];
-    button.textContent = currentLetter;
+    
 };
