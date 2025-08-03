01972b6241129a6e2b48baa51149687bbae6a601
Colin James Nguyen <nguyen_c9@denison.edu>
2025-08-03 21:32:22 +0000

    Display score during game

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ console.log('JavaScript file is linked correctly.');
 const button = document.getElementById('button');
 const body = document.body;
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const gameOver = document.getElementById('game-over');
+const playerScore = document.getElementById('score');
+const restartButton = document.getElementById('restart-button');
+
 let score = 0;
+let gameEnd = false; 
 
 let currentLetter = letters[Math.floor(Math.random() * letters.length)];
 button.textContent = currentLetter;
@@ -18,27 +24,48 @@ function playGame(event) {
     });
 
     if (keyPressed.toUpperCase() === currentLetter.toUpperCase()) {
-        console.log('eureka');
         button.classList.add('correct');
         setTimeout(() => {
             button.classList.remove('correct');
         }, 200);
         score++;
+        playerScore.textContent = score;
         console.log(`Correct! Score: ${score}`);
+        currentLetter = letters[Math.floor(Math.random() * letters.length)];
+        button.textContent = currentLetter;
 
     } else {
         button.classList.add('wrong');
-        
+        setTimeout(() => {
+            button.classList.remove('wrong');
+        }, 200);
+        gameEnd = true;
         console.log(`Wrong! Score: ${score}`);
+        displayGameOver();
+
     }
-    currentLetter = letters[Math.floor(Math.random() * letters.length)];
-    button.textContent = currentLetter;
 
     document.addEventListener('keyup', (e) => {
         button.classList.remove('clicked');
     });
-
-
-
     
 };
+
+function displayGameOver() {
+    playerScore.textContent = score;
+    gameOver.style.display = 'block';
+    restartButton.addEventListener('click', restartGame);
+}
+
+function restartGame() {
+    const gameOver = document.getElementById('game-over');
+    gameOver.style.display = 'none';
+
+    score = 0;
+    gameEnd = false;
+    playGame();
+    let currentLetter = letters[Math.floor(Math.random() * letters.length)];
+    button.textContent = currentLetter;
+    
+}
+
